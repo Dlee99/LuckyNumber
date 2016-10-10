71ccae4b16e8cd9cc7881961f4dbb10557de1745
Dlee99 <Dereklee9ab@aol.com>
2016-10-10 16:07:52 -0400

    Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,33 @@
 package com.company;
 import java.util.Scanner;
-import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Please input your name");
         RandomPerfect randPerf = new RandomPerfect();
+        Scanner sc = new Scanner(System.in);
+        Scanner integer = new Scanner(System.in);
+        String name, userNumber, userList = "";
+        int randInt, x = 1;
+        while(x == 1) {
+            System.out.println("Please input a name");
+            name = sc.nextLine();
+            randInt = randPerf.randomInteger();
+            userNumber = name + "\t\t\t\t" + randInt + "\n";
+            userList = userList + userNumber;
+            System.out.println("Would you like to stop the program (0), input a new name (1), or get a list of names (2)?");
+            x = integer.nextInt();
+            if(x == 2){
+                System.out.println(userList);
+                System.out.println("Would you like to input a new name? Y || N");
+                String newName = sc.nextLine();
+                newName = newName.toLowerCase();
+                if(newName.equals("y")){
+                    x = 1;
+                }
+            }
+            else if(x == 0){
+                System.out.println(userList);
+            }
+        }
     }
 }
--- src/com/company/RandomPerfect.java
+++ src/com/company/RandomPerfect.java
@@ -1,18 +1,14 @@
 package com.company;
 import java.util.Random;
-import java.util.Scanner;
 /**
  * Created by dl447 on 10/10/16.
  */
 public class RandomPerfect {
-    Scanner sc = new Scanner(System.in);
     Random rand = new Random();
-    private String name;
     private int randInt;
-    public String userNumber;
-    public RandomPerfect(){
-        name = sc.nextLine();
-        randInt = rand.nextInt();
-        userNumber = name + "\t" + randInt;
+    public int randomInteger() {
+        randInt = rand.nextInt(1000);
+        randInt = randInt * randInt;
+        return randInt;
     }
 }
